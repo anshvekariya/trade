f7b8ce8fdc37707e18121192fe1fc66496991992
anshvekariya <anshvekariya333@gmail.com>
2024-02-02 18:09:55 +0530

    trade project - ansh

---
+++ src/Contact.js
@@ -0,0 +1,21 @@
+import Acco from "./Acco";
+import Banner from "./Banner";
+import Detail from "./Detail";
+import Detailtwo from "./Detailtwo";
+import Futtor from "./Futtor";
+import Hader from "./Hader";
+
+function Contact() {
+    return(
+        <>
+
+        <Banner/>
+        <Acco/>
+        <Futtor/>
+        
+        
+        </>
+
+    );
+}
+export default Contact
---
+++ src/Home.js
@@ -0,0 +1,43 @@
+import './App.css'
+import Hader from "./Hader"
+import Futtor from './Futtor';
+import Slider from './Slider';
+import Icard from './Icard';
+import Detail from './Detail';
+import Detailtwo from './Detailtwo';
+import Scard from './Scard';
+import Bcard from './Bcard';
+import Acco from './Acco';
+import Sliderowl from './Sliderowl';
+
+
+
+
+function Home() {
+    
+    return(
+        <>
+
+
+
+        <Slider/>
+       
+        <Icard/>
+        <Detail/>
+        <Detailtwo/>
+        <Scard/>
+        <Acco/>
+        <Sliderowl/>
+        
+        <Bcard/>
+       
+
+        <Futtor/>
+
+     
+        </>
+    )
+    
+    }
+    
+    export default Home
